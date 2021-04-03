19c9b80e86093a4de08f7f8767c2c5198f9aa9e1
System Administrator <root@Erics-MacBook-Pro.local>
2021-04-03 19:08:04 +0900

    Add the delopyment network section of gorli_infrua test net

--- truffle-config.js
+++ truffle-config.js
@@ -17,7 +17,13 @@ module.exports = {
         return new HDWallet(process.env.MNEMONIC, "http://127.0.0.1:7545" , accountIndex)
       },
       network_id: 5777
-    }
+    },
+    gorli_infrua: {
+      provider: function() {
+        return new HDWallet(process.env.MNEMONIC, "https://goerli.infura.io/v3/6458cbdc12124744a4be937d7d4c8af7" , accountIndex)
+      },
+      network_id: 5
+    },
   },
   compilers: {
     solc: {
